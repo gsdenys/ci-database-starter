8d4c653d46aeae1acce753493625624db1a447f7
Denys G. Santos <gsdenys@gmail.com>
2022-04-26 13:52:11 -0300

    add methods to exclude db test

--- conn-data/conn.go
+++ conn-data/conn.go
@@ -14,42 +14,62 @@ type Test struct {
 	Args
 }
 
-func GetTestData() []Test {
-	return []Test{
-		{
-			Name: "Clickhouse",
-			Args: Args{
-				Type: database.Clickhouse,
-				DSN:  "tcp://localhost:9000?database=default",
-			},
+const (
+	Clickhouse string = "Clickhouse"
+	MSSQL      string = "MSSQL"
+	MySQL      string = "MySQL"
+	PostgreSQL string = "PostgreSQL"
+	SQLite     string = "SQLite"
+)
+
+var data = []Test{
+	{
+		Name: Clickhouse,
+		Args: Args{
+			Type: database.Clickhouse,
+			DSN:  "tcp://localhost:9000?database=default",
 		},
-		{
-			Name: "MSSQL",
-			Args: Args{
-				Type: database.MSSQL,
-				DSN:  "sqlserver://sa:Adm1n123@localhost:1433?database=master",
-			},
+	},
+	{
+		Name: MSSQL,
+		Args: Args{
+			Type: database.MSSQL,
+			DSN:  "sqlserver://sa:Adm1n123@localhost:1433?database=master",
 		},
-		{
-			Name: "MySQL",
-			Args: Args{
-				Type: database.MySQL,
-				DSN:  "root:mysql@tcp(localhost:3306)/mysql?parseTime=true",
-			},
+	},
+	{
+		Name: MySQL,
+		Args: Args{
+			Type: database.MySQL,
+			DSN:  "root:mysql@tcp(localhost:3306)/mysql?parseTime=true",
 		},
-		{
-			Name: "PostgreSQL",
-			Args: Args{
-				Type: database.Postgres,
-				DSN:  "host=localhost port=5432 user=postgres password=postgres dbname=postgres sslmode=disable",
-			},
+	},
+	{
+		Name: PostgreSQL,
+		Args: Args{
+			Type: database.Postgres,
+			DSN:  "host=localhost port=5432 user=postgres password=postgres dbname=postgres sslmode=disable",
 		},
-		{
-			Name: "SQLite",
-			Args: Args{
-				Type: database.SQLite,
-				DSN:  "test.db",
-			},
+	},
+	{
+		Name: SQLite,
+		Args: Args{
+			Type: database.SQLite,
+			DSN:  "test.db",
 		},
+	},
+}
+
+func RemoveFromTestData(toRemove string, myData []Test) []Test {
+	for index, test := range myData {
+		if test.Name == toRemove {
+			return append(myData[:index], myData[index+1:]...)
+		}
 	}
+
+	return myData
+}
+
+func GetTestData() []Test {
+	return data
 }
---
+++ conn-data/conn_test.go
@@ -0,0 +1,62 @@
+package conndata
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestRemoveFromTestData(t *testing.T) {
+
+	type args struct {
+		toRemove string
+		myData   []Test
+	}
+	tests := []struct {
+		name string
+		args args
+		want []Test
+	}{
+		{
+			name: "nothing-to-do",
+			args: args{
+				toRemove: "no-one",
+				myData:   data,
+			},
+			want: data,
+		},
+		{
+			name: Clickhouse,
+			args: args{
+				toRemove: Clickhouse,
+				myData:   data,
+			},
+			want: append(data[:1], data[2:]...),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := RemoveFromTestData(tt.args.toRemove, tt.args.myData); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("RemoveFromTestData() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGetTestData(t *testing.T) {
+	tests := []struct {
+		name string
+		want []Test
+	}{
+		{
+			name: "full-data",
+			want: data,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := GetTestData(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetTestData() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
